a71d8c2288f1160c3c7e7b6284643b16b9510a98
Drake Smith <drakesmithjr@gmail.com>
2016-11-10 06:52:29 -0800

    calculations done. cleaned up code and added comments. need to complete integrating  data into map

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
+//express dependency, and initialize express
 var express = require('express');
 var app = express();
 
+//dependencies
 var d3 = require('d3');
 var path = require('path');
 var PORT = process.env.PORT || 3000;
 
-//app.use(express.static(process.cwd() + '/public'));
+//serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static('data'));
 
-
-
+//send public 
 app.get('/', function(req, res) {
-    //res.sendFile(path.join(__dirname+'/index.html'));
     res.sendFile('public/index.html');
 });
 
+//app listening on port 3000
 app.listen(PORT, function() {
 	console.log("App listening on Port " + PORT);
 });
